e23fd81422cfa0d053bf8e4b68b065b590531d75
Venkateshwara reddy Gowru <venkateshwarareddy.gowru@Venkateshwaras-MacBook-Pro.local>
2020-04-04 12:35:49 -0400

    Cards Application Complete

--- cards/src/app/app.component.ts
+++ cards/src/app/app.component.ts
@@ -6,5 +6,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'cards';
+  posts = [{
+    title: 'Neat Tree', imageUrl: '../assets/oak-tree-sunset-iStock-477164218-1080x608.jpg',
+    username: 'nature', content: 'I saw this neat tree today'
+  },
+  {
+    title: 'Snowy Mountain', imageUrl: '../assets/mountain-range-appenzell-switzerland.jpg',
+    username: 'mountainlover', content: 'Here is a picutre of a snowy mountain'
+  },
+  {
+    title: 'Mountain Biking', imageUrl: '../assets/biking.jpg',
+    username: 'biking2222', content: 'I did some biking today'
+  }];
 }
